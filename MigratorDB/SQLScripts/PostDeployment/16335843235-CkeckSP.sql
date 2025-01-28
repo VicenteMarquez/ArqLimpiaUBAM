39d316f7d520bcf66a0013ababc143b9f3ecf120
Ramiro Zein <ramiro.zein.desarrollo@outlook.com>
2025-01-28 11:27:31 -0600

    Delete MigratorDB directory

--- MigratorDB/SQLScripts/PostDeployment/16335843235-CkeckSP.sql
+++
@@ -1,9 +0,0 @@
-﻿--16335843235_CkeckSP
---Autor:Vicente Marquez
---Objetivo:Validar los objetos creados Base dde datos 
---Fecha: 2024-09-30
---Comentarios_Aquí de pueden ejectar las pruebas de implementacion
-
-SELECT * FROM Products
-
---16335843235_CkeckSP
